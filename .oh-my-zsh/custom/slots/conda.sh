c6bf512f18b4011e71105116cd15036fd7293ace
lazyboy <lazyboy.fan@gmail.com>
2020-10-16 17:48:53 +0800

    zsh: tab to space

--- .oh-my-zsh/custom/slots/conda.sh
+++ .oh-my-zsh/custom/slots/conda.sh
@@ -2,8 +2,8 @@ CONDA_ENV=$HOME/.conda/envs
 CONDA_MACOS=$HOME/.conda/envs/macOS
 
 function setconda() {
-	if [ -d "$CONDA_ENV/$1" ]; then
-		export PATH=$CONDA_ENV/$1/bin:$PATH
-	fi
+    if [ -d "$CONDA_ENV/$1" ]; then
+        export PATH=$CONDA_ENV/$1/bin:$PATH
+    fi
 }
 setconda macOS
--- .oh-my-zsh/custom/slots/go.sh
+++ .oh-my-zsh/custom/slots/go.sh
@@ -5,12 +5,12 @@ export GOPROXY=https://goproxy.io,direct
 export PATH=$GOPATH/bin:$PATH
 
 function gopractice {
-	# base=$(echo $1|cut -d "." -f1)
-	base="$1"
-	dir="$HOME/Code/Local/practice/go/$base"
-	file="$dir/main.go"
-	if [ ! -d "$dir" ]; then
-		mkdir "$dir"
-	fi
-	touch "$file" && cd "$dir" && code "$file"
+    # base=$(echo $1|cut -d "." -f1)
+    base="$1"
+    dir="$HOME/Code/Local/practice/go/$base"
+    file="$dir/main.go"
+    if [ ! -d "$dir" ]; then
+        mkdir "$dir"
+    fi
+    touch "$file" && cd "$dir" && code "$file"
 }
