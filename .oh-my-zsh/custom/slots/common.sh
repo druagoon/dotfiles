0878fbdf719edfaa95d725e61dc2c4b4951467af
lazyboy <lazyboy.fan@gmail.com>
2020-12-04 10:34:18 +0800

    update $PATH

--- .oh-my-zsh/custom/slots/common.sh
+++ .oh-my-zsh/custom/slots/common.sh
@@ -2,7 +2,7 @@ unset MAILCHECK
 
 export LC_ALL=en_US.UTF-8
 export EDITOR="vim"
-export PATH=$HOME/local/bin:$HOME/bin:/usr/local/sbin:/usr/local/bin:$PATH
+export PATH=$HOME/Local/bin:/usr/local/sbin:/usr/local/bin:$PATH
 
 alias zshrl="source ~/.zshrc"
 alias flushdns="dscacheutil -flushcache;sudo killall -HUP mDNSResponder"
