c05d9c6ed326de680aa21df489275df071b549e3
lazyboy <lazyboy.fan@gmail.com>
2021-09-16 16:14:59 +0800

    [Go]add xgo function

--- .oh-my-zsh/custom/slots/go.sh
+++ .oh-my-zsh/custom/slots/go.sh
@@ -4,10 +4,10 @@ export GO111MODULE=on
 export GOPROXY=https://goproxy.io,direct
 export PATH=$GOPATH/bin:$PATH
 
-function gopractice {
+function xgo {
     # base=$(echo $1|cut -d "." -f1)
     base="$1"
-    dir="$HOME/Code/Local/practice/go/$base"
+    dir="$HOME/Code/Local/practice/x-go/$base"
     file="$dir/main.go"
     if [ ! -d "$dir" ]; then
         mkdir "$dir"
