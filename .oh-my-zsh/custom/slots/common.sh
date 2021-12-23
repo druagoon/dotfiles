feff816099cf478c990c8e4d0723ed3febd3ed6b
druagoon <lazyboy.fan@gmail.com>
2021-12-23 10:28:15 +0800

    update `$PATH`

--- .oh-my-zsh/custom/slots/common.sh
+++ .oh-my-zsh/custom/slots/common.sh
@@ -4,7 +4,7 @@ export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export EDITOR="vim"
 # export HISTTIMEFORMAT="%F %T "
-export PATH=$HOME/.env/macOS/bin:/usr/local/sbin:/usr/local/bin:$PATH
+export PATH=$HOME/.env/macOS/bin:$PATH
 
 alias zshrl="source ~/.zshrc"
 alias flushdns="dscacheutil -flushcache;sudo killall -HUP mDNSResponder"
--- .zprofile
+++ .zprofile
@@ -1,3 +1,5 @@
+export PATH=/usr/local/bin:/usr/local/sbin:$PATH
+
 ZPROFILED="$HOME/.zprofile.d"
 
 if [ -d "$ZPROFILED" ]; then
