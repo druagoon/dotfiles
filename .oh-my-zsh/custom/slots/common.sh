4750121ce18e9339dec06252451ca72f81f45d51
lazyboy <lazyboy.fan@gmail.com>
2022-03-03 12:35:19 +0800

    rename `zshrl` -> `ldzsh`

--- .oh-my-zsh/custom/slots/common.sh
+++ .oh-my-zsh/custom/slots/common.sh
@@ -6,7 +6,7 @@ export EDITOR="vim"
 # export HISTTIMEFORMAT="%F %T "
 export PATH=$HOME/.env/macOS/bin:$PATH
 
-alias zshrl="source ~/.zshrc"
+alias ldzsh="source ~/.zshrc"
 alias flushdns="dscacheutil -flushcache;sudo killall -HUP mDNSResponder"
 alias pb64="pbpaste | base64 -D && echo"
 alias pb64j="pbpaste | base64 -D | jq --indent 4 -S . | pbcopy && echo 'ok'"
