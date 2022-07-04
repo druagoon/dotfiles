323227264d4b48f96125dd42062d7c44863a2d09
lazyboy <lazyboy.fan@gmail.com>
2022-07-04 11:31:11 +0800

    update time2str.py

--- bin/time2str.py
+++ bin/time2str.py
@@ -1,25 +1,37 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """Parse timestamp to RFC 3339
 """
 
 import datetime
 import time
-import warnings
 
+import click
 import clipboard
+from tabulate import tabulate
 
 
+@click.command('time2str', help=__doc__)
 def main():
     ts = str(clipboard.paste())
     try:
         ts = float(ts)
     except:
-        warnings.warn('Invalid timestamp. Use local current timestamp')
         ts = time.time()
     dt = datetime.datetime.fromtimestamp(ts)
-    date = dt.strftime('%Y-%m-%d %H:%M:%S')
-    print('{} -> {}'.format(ts, date))
+    formats = ['%Y-%m-%d %H:%M:%S', '%Y%m%d%H%M%S', '%Y%m%d_%H%M%S']
+    dates = map(lambda x: dt.strftime(x), formats)
+    headers = ['Timestamp', 'F1', 'F2', 'F3']
+    rows = [[ts, *dates]]
+    output = tabulate(
+        rows,
+        headers=headers,
+        tablefmt='grid',
+        floatfmt='.6f',
+        numalign='left',
+        stralign='left'
+    )
+    print(output)
 
 
 if __name__ == '__main__':
