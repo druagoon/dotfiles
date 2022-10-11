50d51c5c000058261a996724847eb3b22d82c8a1
lazyboy <lazyboy.fan@gmail.com>
2022-10-11 15:38:29 +0800

    update slot curl.sh

--- .oh-my-zsh/custom/slots/curl.sh
+++ .oh-my-zsh/custom/slots/curl.sh
@@ -1,5 +1,6 @@
 CURL_FORMAT=$ZSH_CUSTOM_ETC/curl-format.txt
 CURL_UA="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36"
 CURL_MUA="Mozilla/5.0 (iPhone; CPU iPhone OS 11_0_3 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Mobile/15A432 NetType/WIFI"
+alias fcurl='curl -w "@$CURL_FORMAT"'
 alias xcurl='curl -i -w "@$CURL_FORMAT"'
 alias xmcurl='curl -i -A "$CURL_MUA" -w "@$CURL_FORMAT"'
