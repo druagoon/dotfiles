7a5955a3377df836a97d9f4b5f8ecabbd6325da2
lazyboy <lazyboy.fan@gmail.com>
2022-10-11 15:38:07 +0800

    udpate slot go.sh

--- .oh-my-zsh/custom/slots/go.sh
+++ .oh-my-zsh/custom/slots/go.sh
@@ -1,6 +1,7 @@
 # export GOROOT=/usr/local/opt/go/libexec
 export GO111MODULE=on
-export GOPROXY=https://goproxy.io,direct
+# export GOPROXY=https://goproxy.io,direct
+# export GOPROXY=https://goproxy.cn,direct
 export GOPATH=$HOME/.go
 export PATH=$GOPATH/bin:$PATH
 
