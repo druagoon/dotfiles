470a2d14cb9e0911082827667a1e15f27538c709
lazyboy <lazyboy.fan@gmail.com>
2023-01-10 13:58:37 +0800

    add cargo config

--- .oh-my-zsh/custom/slots/rust.sh
+++ .oh-my-zsh/custom/slots/rust.sh
@@ -1,1 +1,9 @@
 [[ -f "$HOME/.cargo/env" ]] && source "$HOME/.cargo/env"
+
+# [rust-static]
+# 上海交通大学
+export RUSTUP_DIST_SERVER=https://mirror.sjtu.edu.cn/rust-static
+export RUSTUP_UPDATE_ROOT=https://mirror.sjtu.edu.cn/rust-static/rustup
+# 字节跳动
+# export RUSTUP_DIST_SERVER="https://rsproxy.cn"
+# export RUSTUP_UPDATE_ROOT="https://rsproxy.cn/rustup"
