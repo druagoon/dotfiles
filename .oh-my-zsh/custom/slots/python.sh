5623b65ed0760c90526be448d51f0e50f06d2906
lazyboy <lazyboy.fan@gmail.com>
2023-02-14 16:51:18 +0800

    feat: update python ldvenv

--- .oh-my-zsh/custom/slots/python.sh
+++ .oh-my-zsh/custom/slots/python.sh
@@ -1,5 +1,19 @@
 # Python
-alias ldvenv="source ./venv/bin/activate"
 alias py27="~/.conda/envs/py27/bin/python"
 alias pip27="~/.conda/envs/py27/bin/pip"
 alias ipy27="~/.conda/envs/py27/bin/ipython"
+
+function ldvenv() {
+    key=$(basename "$(pwd)")
+    VENV_DIRS=("venv" ".venv" "$HOME/.virtualenvs" "$HOME/.conda/envs")
+    for v in "${VENV_DIRS[@]}"; do
+        if [[ -f "$v/bin/activate" ]]; then
+            . "$v/bin/activate"
+            break
+        fi
+        if [[ -f "$v/$key/bin/activate" ]]; then
+            . "$v/$key/bin/activate"
+            break
+        fi
+    done
+}
