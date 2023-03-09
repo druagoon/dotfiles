f6be89f86b2505d8f89af9c53e786148ba734936
lazyboy <lazyboy.fan@gmail.com>
2023-03-10 00:12:22 +0800

    add link.sh

---
+++ link.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+arch="$(uname -m)"
+if [[ "${arch}" == "arm64" ]]; then
+    BREW_PREFIX="/opt/homebrew"
+else
+    BREW_PREFIX="/usr/local"
+fi
+export PATH="${BREW_PREFIX}/bin:${BREW_PREFIX}/sbin:${PATH}"
+
+STOW_SRC="${HOME}/.dotfiles"
+STOW_TARGET="${HOME}"
+
+do_stow() {
+    stow -v -d "${STOW_SRC}" -t "${STOW_TARGET}" "${1}"
+}
+
+link_zsh() {
+    local zfiles=(.zprofile .zshenv .zshrc)
+    for v in "${zfiles[@]}"; do
+        local filepath="${HOME}/${v}"
+        if [[ ! -L "${filepath}" ]]; then
+            rm -f "${filepath}"
+            echo "Delete ${filepath}"
+        fi
+    done
+    do_stow zsh
+}
+
+main() {
+    link_zsh
+}
+
+main
