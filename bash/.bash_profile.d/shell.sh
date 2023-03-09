823ca141ae82b7c57fd1bac2e6e0831c27b77089
lazyboy <lazyboy.fan@gmail.com>
2023-03-09 16:37:00 +0800

    feat: update bash

--- bash/.bash_profile.d/shell.sh
+++ bash/.bash_profile.d/shell.sh
@@ -1,19 +1,42 @@
+# https://en.wikipedia.org/wiki/ANSI_escape_code
+
 BLACK="\033[0;30m"
 RED="\033[0;31m"
+RED_B="\033[1;31m"
 GREEN="\033[0;32m"
 ORANGE="\033[0;33m"
 BLUE="\033[0;34m"
 PURPLE="\033[0;35m"
 CYAN="\033[0;36m"
 LIGHT_GRAY="\033[0;37m"
+LIGHT_GRAY_I="\033[3;37m"
 RESET="\033[0m"
 
-_get_branch_name() {
-    name=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+showcolor() {
+    echo -e "${BLACK} BLACK ${RESET}"
+    echo -e "${RED} RED ${RESET}"
+    echo -e "${GREEN} GREEN ${RESET}"
+    echo -e "${ORANGE} ORANGE ${RESET}"
+    echo -e "${BLUE} BLUE ${RESET}"
+    echo -e "${PURPLE} PURPLE ${RESET}"
+    echo -e "${CYAN} CYAN ${RESET}"
+    echo -e "${LIGHT_GRAY} LIGHT_GRAY ${RESET}"
+}
+
+__get_ip() {
+    ip=""
+    if [[ -x "$(command -v ip)" ]]; then
+        ip=$(ip -o -4 addr list | grep -v "127.0.0.1" | awk '{ip=$2"="$4; print ip}' | cut -d/ -f1 | xargs)
+    fi
+    echo "$ip"
+}
+
+__get_branch_name() {
+    name="$(git rev-parse --abbrev-ref HEAD 2> /dev/null)"
     if [[ -z "${name}" ]]; then
         echo ""
     else
-        echo -e "${BLUE}(${ORANGE}${name}${BLUE})${RESET}"
+        echo -e "${GREEN}(${RED_B}${name}${GREEN})"
     fi
 }
 
@@ -21,12 +44,19 @@ ldprofile() {
     source ~/.bash_profile
 }
 
-ldps1() {
-    export PS1="${RESET}[${RED}\D{%Y/%m/%d} \t ${GREEN}\w${RESET}\$(_get_branch_name)${RESET}]\n[${PURPLE}\u${BLUE}@${CYAN}\H${RESET}]\$ "
+__ps_part() {
+    echo "${RESET}${1}${RESET}"
 }
 
-ldpps1() {
-    export PS1="${RESET}[${RED}\D{%Y/%m/%d} \t ${GREEN}\w${RESET}]\n[${PURPLE}\u${BLUE}@${CYAN}\H${RESET}]\$ "
+ldps1() {
+    time="$(__ps_part "${BLUE}\\D{%Y/%m/%d} \\t")"
+    ip="$(__ps_part "${ORANGE}$(__get_ip)")"
+    cwd="$(__ps_part "${LIGHT_GRAY_I}\\w")"
+    branch="$(__ps_part "\$(__get_branch_name)")"
+    user="$(__ps_part "${PURPLE}\\u")"
+    host="$(__ps_part "${CYAN}\\H")"
+    export PS1="[${time} ${ip} ${cwd}${branch}]\n[${user}@${host}]\$ "
+    # export PS1="${RESET}[${BLUE}\D{%Y/%m/%d} \t ${ORANGE}$(__get_ip) ${GREEN}\w${RESET}\$(__get_branch_name)${RESET}]\n[${PURPLE}\u${BLUE}@${CYAN}\H${RESET}]\$ "
 }
 
 __init_shell_path() {
