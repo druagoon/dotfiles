ef8d9ee2ad3d9e49a5bb63ac9068aa8e2ccf1757
lazyboy <lazyboy.fan@gmail.com>
2023-03-20 13:40:53 +0800

    feat: update bash makefile completion

---
+++ bash/.bash_profile.d/makefile.sh
@@ -0,0 +1,10 @@
+__init_makefile_completion() {
+    # complete -W "\`grep -oE '^[a-zA-Z0-9_.-]+:([^=]|$)' ?akefile | grep -v ".PHONY" | sed 's/[^a-zA-Z0-9_.-]*$//' | sort -u\`" make
+    complete -W "\`make -qp | sed -n -E 's/^([^.#\s][^:=]*)(:$|:\s+.*$)/\1/p' | sort -u\`" make
+}
+
+__init_makefile() {
+    __init_makefile_completion
+}
+
+__init_makefile
