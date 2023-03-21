5a18811482b9a980da343b75e14a119dc3b68cb6
lazyboy <lazyboy.fan@gmail.com>
2023-03-21 23:57:50 +0800

    update go.sh

--- go/.oh-my-zsh/custom/init.d/go.sh
+++ go/.oh-my-zsh/custom/init.d/go.sh
@@ -1,7 +1,5 @@
-# export GOROOT=/usr/local/opt/go/libexec
 export GO111MODULE=on
-# export GOPROXY=https://goproxy.io,direct
-# export GOPROXY=https://goproxy.cn,direct
+export GOPROXY=https://goproxy.cn,direct
 export GOPATH="${HOME}/.go"
 
 __init_go() {
