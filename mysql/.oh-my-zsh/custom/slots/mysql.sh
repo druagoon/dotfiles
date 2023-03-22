27ce319ce315acb11272d8eaa842ec9f2b28fd86
lazyboy <lazyboy.fan@gmail.com>
2023-03-22 11:25:52 +0800

    feat: update mysql path

--- mysql/.oh-my-zsh/custom/slots/mysql.sh
+++ mysql/.oh-my-zsh/custom/slots/mysql.sh
@@ -2,7 +2,13 @@
 export MYSQL_PS1="[\R:\m:\s] (\u@\h:\p) [\d]#\c\n> "
 
 __init_mysql() {
-    df_prepend_path "/usr/local/opt/mysql@5.7/bin"
+    local mysql_prefix="$(brew --repfix mysql@5.7 2> /dev/null)"
+    if [[ -d "${mysql_prefix}" ]]; then
+        local mysql_bin="${mysql_prefix}/bin"
+        if [[ -d "${mysql_bin}" ]]; then
+            df_prepend_path "${mysql_bin}"
+        fi
+    fi
 }
 
 __init_mysql
