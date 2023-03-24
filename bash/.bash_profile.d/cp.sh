c6b4b087f0892f51759c8cf091bcd678dd3bdb2e
lazyboy <lazyboy.fan@gmail.com>
2023-03-24 17:06:39 +0800

    feat: add bash cpv

---
+++ bash/.bash_profile.d/cp.sh
@@ -0,0 +1,10 @@
+cpv() {
+    rsync -pogbr -hhh --backup-dir="/tmp/rsync-${USER}" -e /dev/null --progress "$@"
+}
+
+cpvg() {
+    rsync -pogbr -hhh --exclude '.git' --backup-dir="/tmp/rsync-${USER}" -e /dev/null --progress "$@"
+}
+
+complete -d -f cpv
+complete -d -f cpvg
