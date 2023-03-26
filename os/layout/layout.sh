320862753aec80bb6ce2a097c92f0835b3a8519a
lazyboy <lazyboy.fan@gmail.com>
2023-03-26 22:25:28 +0800

    feat: add comment for clear layout file content

--- os/layout/layout.sh
+++ os/layout/layout.sh
@@ -5,7 +5,10 @@ LAYOUT_DIR="${HOME}/.dotfiles/os/layout"
 layout_git() {
     local filename="$(basename "$1")"
     local filepath="${LAYOUT_DIR}/${filename}.txt"
+
+    # Clear layout file content
     >"${filepath}"
+
     find "$1" -name ".git" ! -path '*/.local/*' | while read name; do
         local target=$(dirname "${name}")
         local url="$(git -C "${target}" config --get remote.origin.url)"
