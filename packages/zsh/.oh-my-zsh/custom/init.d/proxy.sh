c2eb71867ddff31fae829675f43df04a2ea453d7
lazyboy <lazyboy.fan@gmail.com>
2024-03-10 23:03:36 +0800

    feat: add localproxy command and completions

--- packages/zsh/.oh-my-zsh/custom/init.d/proxy.sh
+++ packages/zsh/.oh-my-zsh/custom/init.d/proxy.sh
@@ -6,22 +6,33 @@ DOTF_PROXY_X_HTTPS="http://${DOTF_PROXY_HTTPS}"
 DOTF_PROXY_X_SOCKS5="socks5://${DOTF_PROXY_SOCKS5}"
 DOTF_PROXY_X_SOCKS5H="socks5h://${DOTF_PROXY_SOCKS5}"
 
-setproxy() {
+__localproxy_set() {
     export http_proxy="${DOTF_PROXY_X_HTTP}"
     export https_proxy="${DOTF_PROXY_X_HTTPS}"
     export all_proxy="${DOTF_PROXY_X_SOCKS5}"
 }
 
-unsetproxy() {
+__localproxy_unset() {
     unset http_proxy https_proxy all_proxy
 }
 
-showproxy() {
+__localproxy_show() {
     echo "http_proxy: ${http_proxy} https_proxy: ${https_proxy} all_proxy: ${all_proxy}"
 }
 
+localproxy() {
+    case "$1" in
+        "set") __localproxy_set ;;
+        "unset") __localproxy_unset ;;
+        "show") __localproxy_show ;;
+        *) echo "Usage: localproxy [set|unset|show]" ;;
+    esac
+}
+
+complete -W "set unset show" localproxy
+
 __init_proxy() {
-    setproxy
+    localproxy set
 }
 
 __init_proxy
