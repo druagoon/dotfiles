24731347603f46e0ef17b460fcc97fc5e11465f4
lazyboy <lazyboy.fan@gmail.com>
2024-05-24 12:14:46 +0800

    fix: command `register-python-argcomplete` not found

--- packages/pipx/.oh-my-zsh/custom/completions/_pipx.zsh
+++ packages/pipx/.oh-my-zsh/custom/completions/_pipx.zsh
@@ -1,3 +1,6 @@
 if command -v pipx &>/dev/null; then
+    if ! command -v register-python-argcomplete &>/dev/null; then
+        pipx install argcomplete
+    fi
     eval "$(register-python-argcomplete pipx)"
 fi
