383fa543b0bb63439df314cbd0bd199ff3dc7dd1
lazyboy <lazyboy.fan@gmail.com>
2025-01-20 18:48:36 +0800

    feat(dotf): support specifying plugin name

--- src/commands/package/new.sh
+++ src/commands/package/new.sh
@@ -1,5 +1,6 @@
 # @cmd Create a new dotfiles package
 # @arg name!
+# @option    --omz-plugin-name <NAME>       Name of the oh-my-zsh plugin
 # @flag    --no-omz-plugins                 Do not create oh-my-zsh plugins directory
 # @flag    --no-omz-functions               Do not create oh-my-zsh functions directory
 # @flag    --no-omz-completions             Do not create oh-my-zsh completions directory
@@ -16,7 +17,7 @@ package::new() {
         dotf::path::gitkeep::ensure_dir "${pkg_omz_custom_dir}/functions"
     fi
     if std::bool::is_false "${argc_no_omz_plugins}"; then
-        local pkg_omz_plugin_name="${DOTF_PLUGIN_PREFIX}-${name}"
+        local pkg_omz_plugin_name="${argc_omz_plugin_name:-"${DOTF_PLUGIN_PREFIX}-${name}"}"
         local pkg_omz_plugin_dir="${pkg_omz_custom_dir}/plugins/${pkg_omz_plugin_name}"
         dotf::path::gitkeep::ensure_dir "${pkg_omz_plugin_dir}/bin"
 
