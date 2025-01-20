c400c3f97a9cf9e47233ea9c40f1684f2d1a930b
lazyboy <lazyboy.fan@gmail.com>
2025-01-20 17:41:51 +0800

    feat(conda): add `dotf-conda` plugin

---
+++ packages/conda/.oh-my-zsh/custom/plugins/dotf-conda/dotf-conda.plugin.zsh
@@ -0,0 +1,8 @@
+CONDA_ENVS_DIR="${HOME}/.conda/envs"
+
+set_conda_env_path() {
+    local path="$CONDA_ENVS_DIR/$1"
+    if [[ -d "${path}" ]]; then
+        _dotf::cmd::path::prepend "${path}/bin"
+    fi
+}
