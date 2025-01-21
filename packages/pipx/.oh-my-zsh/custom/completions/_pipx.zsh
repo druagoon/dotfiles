2c1be8411226d9fe3166712fc11b7e48420b505f
lazyboy <lazyboy.fan@gmail.com>
2025-01-21 17:30:54 +0800

    feat(pipx): remove pipx package

--- packages/pipx/.oh-my-zsh/custom/completions/_pipx.zsh
+++
@@ -1,6 +0,0 @@
-if _dotf::cmd::check pipx; then
-    if ! _dotf::cmd::check register-python-argcomplete; then
-        brew install python-argcomplete
-    fi
-    eval "$(register-python-argcomplete pipx)"
-fi
