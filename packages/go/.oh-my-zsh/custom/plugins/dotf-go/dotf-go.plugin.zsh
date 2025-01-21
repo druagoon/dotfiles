ec648099258309f7e20e6b5eae6d14c8720f3b6d
lazyboy <lazyboy.fan@gmail.com>
2025-01-21 16:26:52 +0800

    feat(go): add `dotf-go` plugin

--- packages/go/.oh-my-zsh/custom/plugins/dotf-go/dotf-go.plugin.zsh
+++ packages/go/.oh-my-zsh/custom/plugins/dotf-go/dotf-go.plugin.zsh
@@ -2,8 +2,12 @@ export GO111MODULE=on
 export GOPROXY=https://goproxy.cn,direct
 export GOPATH="${HOME}/.go"
 
-__init_go() {
+__dotf_go_env_path() {
     _dotf::cmd::path::prepend "${GOPATH}/bin"
 }
 
-__init_go
+__dotf_go_init() {
+    __dotf_go_env_path
+}
+
+__dotf_go_init
