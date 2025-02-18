22ff83c53aa43d4ec65d3758b40f20d11741910d
lazyboy <lazyboy.fan@gmail.com>
2025-02-18 17:05:45 +0800

    feat(git): move `dotf-git-prompt` plugin to `git` package and replace with extended functionality

---
+++ packages/git/.oh-my-zsh/custom/plugins/dotf-git-prompt/dotf-git-prompt.plugin.zsh
@@ -0,0 +1,13 @@
+# Extend oh-my-zsh git-prompt plugin
+# https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/git-prompt
+
+function preexec_dotf_update_git_vars() {
+    case "$2" in
+        make*)
+            __EXECUTED_GIT_COMMAND=1
+            ;;
+    esac
+}
+
+autoload -U add-zsh-hook
+add-zsh-hook preexec preexec_dotf_update_git_vars
