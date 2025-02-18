4f74a297f5a05fa8e4bd7ee82d771a8d12bf74e6
lazyboy <lazyboy.fan@gmail.com>
2025-02-18 15:58:52 +0800

    feat(brew): add alias for cask search and configure Homebrew auto-update interval

--- packages/brew/.oh-my-zsh/custom/plugins/dotf-brew/dotf-brew.plugin.zsh
+++ packages/brew/.oh-my-zsh/custom/plugins/dotf-brew/dotf-brew.plugin.zsh
@@ -1,3 +1,7 @@
 alias bs="brew search"
 alias bi="brew info"
+alias bcs="brew search --cask"
 alias bcog="brew outdated --cask --greedy"
+
+# export HOMEBREW_NO_ANALYTICS="1"
+export HOMEBREW_AUTO_UPDATE_SECS="86400"
