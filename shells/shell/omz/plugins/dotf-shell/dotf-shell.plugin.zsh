d3a4ee45539bdc2d8cc483787dac0e9fec32a729
lazyboy <lazyboy.fan@gmail.com>
2025-08-02 22:53:30 +0800

    feat(dotf-shell): add alias `clhist`

--- shells/shell/omz/plugins/dotf-shell/dotf-shell.plugin.zsh
+++ shells/shell/omz/plugins/dotf-shell/dotf-shell.plugin.zsh
@@ -4,19 +4,12 @@ alias ll='ls -lh'
 alias la='ls -lAh'
 
 alias ldsh='source ~/.zshrc'
+alias clhist='yes | history -c'
 alias flushdns='dscacheutil -flushcache; sudo killall -HUP mDNSResponder'
 alias ppath='echo "$PATH" | sed -e "s/:/\\n/g" | sed -e "s#^${HOME}#~#"'
 alias pfpath='print -l $fpath | sed "s#^${HOME}#~#g"'
 
-__dotf_shell_init_terminal_tabstop() {
-    # Set tabstop to 4 spaces
-    if command -v tabs >/dev/null 2>&1; then
-        tabs -4
-    fi
-}
-
-__dotf_shell_init() {
-    __dotf_shell_init_terminal_tabstop
-}
-
-__dotf_shell_init
+# Set tabstop to 4 spaces
+if command -v tabs >/dev/null 2>&1; then
+    tabs -4
+fi
