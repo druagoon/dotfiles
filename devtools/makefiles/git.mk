b65ec3f03cdb486462b7871cf25236600ce9f1ed
lazyboy <lazyboy.fan@gmail.com>
2026-05-26 11:33:09 +0800

    refactor: consolidate git helpers
    Moves commit helper aliases into git config and renames push/pull make targets for clearer, more consistent naming.

--- devtools/makefiles/git.mk
+++ devtools/makefiles/git.mk
@@ -5,29 +5,17 @@ GIT_REMOTE ?= origin## Git remote repository alias
 
 ##@ Git
 
-.PHONY: reset
-reset: ## Git reset with `--hard HEAD` then discard local changes
-	$(GIT) reset --hard HEAD
-
-.PHONY: amend
-amend: ## Git commit with --amend
-	$(GIT) commit --amend --no-edit
-
-.PHONY: reuse
-reuse: ## Git commit with --reuse-message=HEAD
-	$(GIT) commit --reuse-message=HEAD
-
-.PHONY: psup
-psup: ## Git push current branch to remote with `--set-upstream`
-	$(eval cb := $(shell $(GIT) rev-parse --abbrev-ref HEAD))
-	$(GIT) push $(GIT_REMOTE) --set-upstream $(cb)
+# .PHONY: push-upstream
+# push-upstream: ## Git push current branch to remote with `--set-upstream`
+# 	$(eval cb := $(shell $(GIT) rev-parse --abbrev-ref HEAD))
+# 	$(GIT) push $(GIT_REMOTE) --set-upstream $(cb)
 
 .PHONY: push
 push: ## Git push to remote
 	$(GIT) push $(GIT_REMOTE)
 
-.PHONY: pushf
-pushf:  ## Git force push with `--force-with-lease`
+.PHONY: push-force
+push-force:  ## Git force push with `--force-with-lease`
 	$(GIT) push $(GIT_REMOTE) --force-with-lease
 
 .PHONY: pull
@@ -35,8 +23,8 @@ pull: ## Git pull with `--prune` and `--rebase`
 	@if [[ -n "$(branch)" ]]; then $(GIT) checkout "$(branch)"; fi
 	$(GIT) pull $(GIT_REMOTE) --prune --rebase
 
-.PHONY: pullf
-pullf: ## Fetch to the latest then use remote to override local branch
+.PHONY: pull-force
+pull-force: ## Fetch to the latest then use remote to override local branch
 	$(eval cb := $(shell $(GIT) rev-parse --abbrev-ref HEAD))
 	$(GIT) fetch && $(GIT) reset --hard $(GIT_REMOTE)/$(cb)
 
